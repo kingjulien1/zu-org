e05ff7dd4d4b493f02f275ee4ba64fdc6b3a2435
julian-blaschke <julianblaschke@icloud.com>
2022-11-29 06:56:25 +0100

    design draft

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 import intervalToDuration from "date-fns/intervalToDuration";
+import { useEffect, useState } from "react";
+import { useCountdown } from "usehooks-ts";
 
 /**
  * @todo change state if countdown has expired
@@ -16,8 +18,17 @@ export async function getServerSideProps() {
 /* eslint-disable react/no-children-prop */
 export default function Home({ countdown }) {
   return (
-    <main className="grid min-h-screen items-center justify-center">
-      <Countdown {...countdown} />
+    <main className="min-h-screen grid grid-cols-3 grid-rows-3 items-center justify-center">
+      <div className="grid items-center px-10 lg:px-20 h-full bg-base-200 col-span-3 md:col-span-2">
+        <LineUp />
+      </div>
+      <div className="grid items-center justify-center col-span-3 row-span-1">
+        <Countdown {...countdown} />
+      </div>
+      <div className="hidden md:block" />
+      <div className="grid items-center text-right px-10 lg:px-20 h-full bg-base-200 col-span-3 md:col-span-2">
+        <Info />
+      </div>
     </main>
   );
 }
@@ -26,33 +37,84 @@ export default function Home({ countdown }) {
  * *********************** sections ***********************
  */
 
-function Countdown({ days, hours, minutes, seconds }) {
+function Countdown({ days: startDays, hours: startHours, minutes: startMinutes, seconds: startSeconds }) {
+  const [days, setDays] = useState(startDays);
+  const [hours, setHours] = useState(startHours);
+  const [minutes, setMinutes] = useState(startMinutes);
+  const [seconds, { startCountdown, resetCountdown }] = useCountdown({ countStart: minutes === startMinutes ? startSeconds : 60, intervalMs: 1000 });
+
+  // start the countdown on page load
+  useEffect(() => startCountdown(), []);
+
+  // countdown logic
+  useEffect(() => {
+    if (seconds === 0) {
+      if (minutes === 0) {
+      } else setMinutes(minutes - 1);
+      if (hours === 0) {
+      } else setHours(hours - 1);
+      if (days === 0) {
+      } else setDays(days - 1);
+      resetCountdown();
+      startCountdown();
+    }
+  }, [seconds, resetCountdown, startCountdown, minutes, hours, days]);
+
+  // wa
+  useEffect(() => {});
+
   return (
     <div className="grid grid-flow-col gap-5 text-center auto-cols-max">
-      <div className="flex flex-col">
-        <span className="countdown font-mono text-5xl">
-          <span style={{ "--value": days }}></span>
-        </span>
-        days
-      </div>
-      <div className="flex flex-col">
-        <span className="countdown font-mono text-5xl">
-          <span style={{ "--value": hours }}></span>
-        </span>
-        hours
-      </div>
-      <div className="flex flex-col">
-        <span className="countdown font-mono text-5xl">
-          <span style={{ "--value": minutes }}></span>
-        </span>
-        min
-      </div>
-      <div className="flex flex-col">
-        <span className="countdown font-mono text-5xl">
-          <span style={{ "--value": seconds }}></span>
-        </span>
-        sec
-      </div>
+      <CountDownValue label="days" value={days} />
+      <CountDownValue label="hours" value={hours} />
+      <CountDownValue label="minutes" value={minutes} />
+      <CountDownValue label="seconds" value={seconds} />
+    </div>
+  );
+}
+
+function CountDownValue({ label, value }) {
+  return (
+    <div className="flex flex-col">
+      <span className="countdown font-mono text-5xl md:text-6xl lg:text-7xl xl:text-9xl">
+        <span style={{ "--value": value }}></span>
+      </span>
+      {label}
+    </div>
+  );
+}
+
+function LineUp() {
+  return (
+    <div className="space-y-6 lg:space-y-12">
+      <h1 className="text-2xl lg:text-5xl">Lineup</h1>
+      <ul className="ml-4 text-sm lg:text-lg space-y-4">
+        <li>
+          <i className="text-accent">[Artist]</i> dj4base
+        </li>
+        <li>
+          <i className="text-accent">[Artist]</i> Zucce
+        </li>
+        <li>
+          <i className="text-accent">[Artist]</i> ...
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+function Info() {
+  return (
+    <div className="space-y-6 lg:space-y-12">
+      <h1 className="text-2xl lg:text-5xl">Infos</h1>
+      <ul className="ml-4 text-sm lg:text-lg space-y-4">
+        <li>
+          Saag 10, Techelsberg <i className="text-accent">[Locaiton]</i>
+        </li>
+        <li>
+          07 December, 2022 <i className="text-accent">[Date]</i>
+        </li>
+      </ul>
     </div>
   );
 }
